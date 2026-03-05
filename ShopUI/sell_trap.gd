1a1a93ef411ddef0194433523eb95ba94ed91434
zach-paronto <97645671+zach-paronto@users.noreply.github.com>
2026-03-04 23:36:32 -0600

    made the ugliest looking shop possible

---
+++ ShopUI/sell_trap.gd
@@ -0,0 +1,28 @@
+extends Control
+class_name SellTrap
+
+@export var trap_frame: TextureRect
+var hovering: bool
+
+func _process(_delta):
+	if is_mouse_over_card():
+		hovering = true
+		trap_frame.scale = Vector2(1.2, 1.2)
+	else:
+		hovering = false
+		trap_frame.scale = Vector2(1, 1)
+
+#Detects if mouse is over card sprite
+func is_mouse_over_card() -> bool:
+	var mouse_pos: Vector2 = get_global_mouse_position()
+	var sprite_rect = Rect2(trap_frame.global_position, trap_frame.texture.get_size())
+	return sprite_rect.has_point(mouse_pos)
+
+func _input(event) -> void:
+	#Detects if mouse is hovering and clicks
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.pressed and hovering:
+			#TODO: on click, add logic to check the amount of resources a player has
+			#if enough, spawn the trap and allow them to drag
+			#else do nothing
+			pass
---
+++ ShopUI/shop_scene.gd
@@ -0,0 +1,11 @@
+extends Control
+class_name Shop
+#https://www.youtube.com/watch?v=NxDAJuO1IzI
+@export var sell_trap: PackedScene
+@export var trap_container: VBoxContainer
+
+func _ready() -> void:
+ #Create 5 sell traps
+	for i in range(10):
+		var sell_trap_instance = sell_trap.instantiate()
+		trap_container.add_child(sell_trap_instance)
