dee1e4ddb33c919bc4bf4033ea30f30face70071
zach-paronto <97645671+zach-paronto@users.noreply.github.com>
2026-03-07 01:02:09 -0600

    trap spawning tweak

--- ShopUI/sell_trap.gd
+++ ShopUI/sell_trap.gd
@@ -4,22 +4,15 @@ class_name SellTrap
 @export var trap_frame: TextureRect
 @export var trap_type: PackedScene
 @export var cost: int
-var hovering: bool
 var tree_root: Node
 
 func _ready():
 	tree_root = get_tree().root
 
-#Detects if mouse is over card sprite
-func is_mouse_over_card() -> bool:
-	var mouse_pos: Vector2 = get_global_mouse_position()
-	var sprite_rect = Rect2(trap_frame.global_position, trap_frame.texture.get_size())
-	return sprite_rect.has_point(mouse_pos)
-
 func _input(event) -> void:
 	#Detects if mouse is hovering and clicks
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-		if event.pressed and hovering:
+		if event.pressed:
 			#TODO: on click, add logic to check the amount of resources a player has
 			#if (player.money >= cost):
 			var sell_trap_instance = trap_type.instantiate()
