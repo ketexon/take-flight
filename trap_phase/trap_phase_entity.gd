e3e32e5182314eded90e7e7b29bb1b5a2f620391
ketexon <zane.a.s.clark@gmail.com>
2026-03-07 23:13:50 -0600

    fixed theme breaking, added character death animation

--- trap_phase/trap_phase_entity.gd
+++ trap_phase/trap_phase_entity.gd
@@ -10,12 +10,40 @@ signal killed
 
 var dead := false
 
+var _death_velocity := Vector2(500, -500)
+const _death_velocity_min := Vector2(400, -300)
+const _death_velocity_max := Vector2(800, -800)
+const _death_gravity := 300.0
+var _death_rot_velocity := 2
 
 func kill() -> void:
 	dead = true
 	on_killed()
 	killed.emit()
+	
+	_calculate_death_velocity()
+	
+	
+func _calculate_death_velocity() -> void:
+	_death_velocity = lerp(
+		_death_velocity_min,
+		_death_velocity_max,
+		randf()
+	)
+	if randf() > 0.5:
+		_death_velocity.x *= -1
+	if randf() > 0.5:
+		_death_rot_velocity *= -1
 
 
 func on_killed() -> void:
 	pass
+
+
+func _process(delta: float) -> void:
+	if not dead:
+		return
+	
+	position += _death_velocity * delta
+	_death_velocity.y += gravity * delta
+	rotation += _death_rot_velocity * delta
